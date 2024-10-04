dee9de5297f0a8ab832c996dba844976e0ecaa01
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2024-10-04 15:03:09 +0300

    added folderrs

--- src/main/java/com/bsu/HandmadeMarketplace/controllers/OrderController.java
+++ src/main/java/com/bsu/HandmadeMarketplace/controllers/OrderController.java
@@ -1,4 +1,4 @@
-package com.bsu.HandmadeMarketplace;
+package com.bsu.HandmadeMarketplace.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
