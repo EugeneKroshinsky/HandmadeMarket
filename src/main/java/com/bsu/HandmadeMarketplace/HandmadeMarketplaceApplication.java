bd59da5eb2e879076583fe2bafefdac8ce2603a0
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2024-10-04 11:35:44 +0300

    test

--- src/main/java/com/bsu/HandmadeMarketplace/HandmadeMarketplaceApplication.java
+++ src/main/java/com/bsu/HandmadeMarketplace/HandmadeMarketplaceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HandmadeMarketplaceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HandmadeMarketplaceApplication.class, args);
+		SpringAparketplaceApplication.class, args);
 	}
 
 }
